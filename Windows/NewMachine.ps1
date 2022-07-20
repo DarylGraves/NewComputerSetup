8d489f46fd87adefb2fa41e42d67e15a28ece2a9
Daryl Graves <darylgraves0309@gmail.com>
2022-07-20 23:59:12 +0100

    Configured PowerToys Keyboard Manager

--- Windows/NewMachine.ps1
+++ Windows/NewMachine.ps1
@@ -2,8 +2,10 @@
 #TODO: Prompt whether to join Domain and do so if requested (don't forget about renaming machine)
 #TODO: Prompt User for Git Username
 #TODO: Prompt User for Git Email
-#TODO~ Configure Git
 #TODO: Look into automating VSCode Settings Sync more. I don't think it's possible though.
+#TODO: If not joining to the domain, add other apps (Brave, Whatsapp, Discord for example)
+
+$Path = Split-Path -Path $MyInvocation.MyCommand.Path 
 
 #region Install Software through Winget
 $AppsToInstall = @(
@@ -15,7 +17,6 @@ $AppsToInstall = @(
 )
 
 $CurrentlyInstalled = winget.exe list
-
 foreach ($Application in $AppsToInstall) {
     if (($CurrentlyInstalled | Select-String -pattern $Application).Matches.Count -eq 0) {
         Write-Host "Installing $Application..." -ForegroundColor Yellow
@@ -28,6 +29,21 @@ foreach ($Application in $AppsToInstall) {
 }
 #endregion Install Software through Winget
 
+#region Copying Config Files
+#PowerToys - Keyboard Mapper
+$Source = $Path + "\ConfigFiles\PowerToys\KeyboardManager\Default.json"
+$Destination = "$ENV:USERPROFILE\AppData\Local\Microsoft\PowerToys\Keyboard Manager\Default.json"
+Copy-Item -Path $Source -Destination $Destination -Force
+
+#TODO: PowerToys Always On Top Config
+#TODO: Any other PowerToys Configs?
+#TODO: Windows Terminal Config Files
+
+#endregion Copying Config Files
+
+#region Running Commands
+#TODO: Configure Git
+#endregion Running Commands
 
 #region Install RSAT
 # Get-WindowsCapability -Name "*RSAT*" -Online | Add-WindowsCapability -Online
