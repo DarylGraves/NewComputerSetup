da213ae843b759e9715a111dafa89c4846d45a9f
Daryl Graves <darylgraves0309@gmail.com>
2022-07-22 00:20:13 +0100

    SysInternals Installation Added

--- Windows/NewMachine.ps1
+++ Windows/NewMachine.ps1
@@ -5,6 +5,7 @@
 #TODO: Look into automating VSCode Settings Sync more. I don't think it's possible though.
 #TODO: If not joining to the domain, add other apps (Brave, Whatsapp, Discord for example)
 
+$ProgressPreference = 'SilentlyContinue' # Stops web request loading bars clogging the output
 $Path = Split-Path -Path $MyInvocation.MyCommand.Path 
 
 #region Install Software through Winget
@@ -45,6 +46,32 @@ Copy-Item -Path $Source -Destination $Destination -Force
 #TODO: Configure Git
 #endregion Running Commands
 
+#region Install SysInternals
+Write-Host "Installing SysInternals..." -ForegroundColor Green
+$Url = "https://download.sysinternals.com/files/SysinternalsSuite.zip"
+$SourcePath = "C:\ToolBox\SysInternals"
+
+if((Test-Path -Path "C:\Toolbox\SysInternals") -ne $True)
+{ 
+    New-Item -Path "C:\Toolbox" -Name "SysInternals" -ItemType Directory -Force | Out-Null 
+}
+
+try {    
+    Invoke-WebRequest -Uri $Url -OutFile "$SourcePath\Sysinternalssuite.zip"
+    Expand-Archive -Path "$SourcePath\Sysinternalssuite.zip" -DestinationPath "$SourcePath\"
+    Remove-Item "$SourcePath\Sysinternalssuite.zip"
+}
+catch {
+    Write-Host "Error occured trying to retrieve and unzip SysInternals" -ForegroundColor Red
+}
+
+if((Get-ChildItem -Path $SourcePath).Count -gt 0)
+{
+    Write-Host "SysInternals installed!" -ForegroundColor Green
+}
+#TODO: Add SysInternals directory to Path
+
+#endregion Install SysInternals
 #region Install RSAT
 # Get-WindowsCapability -Name "*RSAT*" -Online | Add-WindowsCapability -Online
 #endregion Install RSAT
