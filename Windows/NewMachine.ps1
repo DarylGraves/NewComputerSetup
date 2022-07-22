d7ae2ca4f18e2ae01dc412b95a4e48ebb5f3e62b
Daryl Graves <darylgraves0309@gmail.com>
2022-07-22 21:22:17 +0100

    Encapsulating Existing Code for cleanliness

--- Windows/NewMachine.ps1
+++ Windows/NewMachine.ps1
@@ -8,7 +8,6 @@
 $ProgressPreference = 'SilentlyContinue' # Stops web request loading bars clogging the output
 $Path = Split-Path -Path $MyInvocation.MyCommand.Path 
 
-#region Install Software through Winget
 $AppsToInstall = @(
     "Microsoft.Powershell"
     "Microsoft.PowerToys"
@@ -17,24 +16,8 @@ $AppsToInstall = @(
     "Git.Git"
 )
 
-$CurrentlyInstalled = winget.exe list
-foreach ($Application in $AppsToInstall) {
-    if (($CurrentlyInstalled | Select-String -pattern $Application).Matches.Count -eq 0) {
-        Write-Host "Installing $Application..." -ForegroundColor Yellow
-        winget.exe install $Application
-        Write-Host "$Application should now be installed!" -ForegroundColor Green
-    }
-    else {
-        Write-Host "$Application already installed, skipping..." -ForegroundColor Green
-    }
-}
-#endregion Install Software through Winget
 
 #region Copying Config Files
-#PowerToys - Keyboard Mapper
-$Source = $Path + "\ConfigFiles\PowerToys\KeyboardManager\Default.json"
-$Destination = "$ENV:USERPROFILE\AppData\Local\Microsoft\PowerToys\Keyboard Manager\Default.json"
-Copy-Item -Path $Source -Destination $Destination -Force
 
 #TODO: PowerToys Always On Top Config
 #TODO: Any other PowerToys Configs?
@@ -46,32 +29,68 @@ Copy-Item -Path $Source -Destination $Destination -Force
 #TODO: Configure Git
 #endregion Running Commands
 
-#region Install SysInternals
-Write-Host "Installing SysInternals..." -ForegroundColor Green
-$Url = "https://download.sysinternals.com/files/SysinternalsSuite.zip"
-$SourcePath = "C:\ToolBox\SysInternals"
-
-if((Test-Path -Path "C:\Toolbox\SysInternals") -ne $True)
-{ 
-    New-Item -Path "C:\Toolbox" -Name "SysInternals" -ItemType Directory -Force | Out-Null 
-}
-
-try {    
-    Invoke-WebRequest -Uri $Url -OutFile "$SourcePath\Sysinternalssuite.zip"
-    Expand-Archive -Path "$SourcePath\Sysinternalssuite.zip" -DestinationPath "$SourcePath\"
-    Remove-Item "$SourcePath\Sysinternalssuite.zip"
-}
-catch {
-    Write-Host "Error occured trying to retrieve and unzip SysInternals" -ForegroundColor Red
-}
-
-if((Get-ChildItem -Path $SourcePath).Count -gt 0)
-{
-    Write-Host "SysInternals installed!" -ForegroundColor Green
-}
-#TODO: Add SysInternals directory to Path
 
 #endregion Install SysInternals
 #region Install RSAT
-# Get-WindowsCapability -Name "*RSAT*" -Online | Add-WindowsCapability -Online
 #endregion Install RSAT
+
+function Install-Applications {
+    param (
+        [String[]]$AppsToInstall
+        )
+        
+    $CurrentlyInstalled = winget.exe list
+        
+    foreach ($Application in $AppsToInstall) {
+        if (($CurrentlyInstalled | Select-String -pattern $Application).Matches.Count -eq 0) {
+            Write-Host "Installing $Application..." -ForegroundColor Yellow
+            winget.exe install $Application
+            Write-Host "$Application should now be installed!" -ForegroundColor Green
+        }
+        else {
+            Write-Host "$Application already installed, skipping..." -ForegroundColor Green
+        }
+    }
+}
+    
+function Install-SysInternals {
+    Write-Host "Installing SysInternals..." -ForegroundColor Green
+    $Url = "https://download.sysinternals.com/files/SysinternalsSuite.zip"
+    $SourcePath = "C:\ToolBox\SysInternals"
+    
+    if((Test-Path -Path "C:\Toolbox\SysInternals") -ne $True)
+    { 
+        New-Item -Path "C:\Toolbox" -Name "SysInternals" -ItemType Directory -Force | Out-Null 
+    }
+    
+    try {    
+        Invoke-WebRequest -Uri $Url -OutFile "$SourcePath\Sysinternalssuite.zip"
+        Expand-Archive -Path "$SourcePath\Sysinternalssuite.zip" -DestinationPath "$SourcePath\" -Force
+        Remove-Item "$SourcePath\Sysinternalssuite.zip"
+    }
+    catch {
+        Write-Host "Error occured trying to retrieve and unzip SysInternals" -ForegroundColor Red
+    }
+    
+    if((Get-ChildItem -Path $SourcePath).Count -gt 0)
+    {
+        Write-Host "SysInternals installed!" -ForegroundColor Green
+    }
+    #TODO: Add SysInternals directory to Path
+}
+
+function Install-RsatTools {
+    Get-WindowsCapability -Name "*RSAT*" -Online | Add-WindowsCapability -Online
+}
+
+function Set-PowerToysConfigFiles {
+    # Keyboard Mapper
+    $Source = $Path + "\ConfigFiles\PowerToys\KeyboardManager\Default.json"
+    $Destination = "$ENV:USERPROFILE\AppData\Local\Microsoft\PowerToys\Keyboard Manager\Default.json"
+    Copy-Item -Path $Source -Destination $Destination -Force
+}
+
+Install-Applications -AppsToInstall $AppsToInstall
+Install-SysInternals
+Install-RsatTools
+Set-PowerToysConfigFiles
