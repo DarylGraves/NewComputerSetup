d0682996e5f5276adc6d713d8081c578cce8be1e
Daryl Graves <darylgraves0309@gmail.com>
2022-08-17 18:46:34 +0100

    Apps & Fonts Complete

---
+++ Windows/NewMachine.ps1
@@ -0,0 +1,102 @@
+# Global Variables
+$Global:ScriptPath = Split-Path -Path $MyInvocation.MyCommand.Path
+$Global:OperatingSystem = (
+            (Get-CimInstance -ClassName Win32_OperatingSystem).Caption |
+            Select-String "Windows \d\d").Matches.Value
+
+$Fonts_File = $Global:ScriptPath + "\Customisations\FontsToInstall.txt"
+$Apps_EssentialsFile = $Global:ScriptPath + "\Customisations\App_Essentials.txt"
+$Apps_PersonalFile = $Global:ScriptPath + "\Customisations\App_Personal.txt"
+
+# Global Variable Debugging
+Write-Debug "Script running from $ScriptPath"
+Write-Debug "OS detected: $Global:OperatingSystem"
+Write-Debug "Essential Apps Path: $Apps_EssentialsFile"
+Write-Debug "Personal Apps Path: $Apps_PersonalFile"
+
+# Validation Steps
+if ($Global:OperatingSystem -eq $Null) {
+    Write-Error "This script will only run on Windows machines." 
+    Exit
+}
+
+try {
+    Get-Command winget -ErrorAction SilentlyContinue | Out-Null
+}
+catch {
+    Write-Error "The machine is currently missing winget.exe - Please run all Windows Updates first and then re-run."
+    Exit
+}
+
+# Initial Prepwork
+function Create-TempFolder {
+    if ((Test-Path "C:\temp") -ne $true) {
+        New-Item -Path "C:\temp" -ItemType Directory | Out-Null
+    }
+
+    Set-Location -Path "C:\temp"
+}
+
+function Install-Fonts {
+    param(
+        [String]$FromFile
+    )
+    Write-Host "Installing Fonts..." -ForegroundColor Green
+
+    $FontUrls = Get-Content -Path $FromFile | Where-Object { $_[0] -ne "#" -and $null -ne $_[0] }
+    $TempFolder = "C:\Windows\Temp\Fonts"
+    $FontInstaller = (New-Object -ComObject Shell.Application).Namespace(0x14) # Don't ask...
+
+    foreach ($Font in $FontUrls) {
+        Invoke-WebRequest -Uri $Font -OutFile "C:\temp\$(Split-Path $Font -leaf)"
+        Expand-Archive -Path "C:\temp\$(Split-Path $Font -leaf)" -DestinationPath $TempFolder -Force
+
+        Get-ChildItem -path $TempFolder | ForEach-Object {
+            $Font = $_.FullName
+            $FontInstaller.CopyHere($Font,0x10)
+            Remove-Item $Font -Force
+        }
+    }
+
+    Get-Childitem "C:\temp" | Remove-Item -Force
+}
+
+# Application Installs
+function Install-Applications {
+    param (
+        [String]$FromFile
+    )
+    
+    if ((Test-Path -Path $FromFile -ErrorAction SilentlyContinue) -ne $True) {
+        Write-Error "The following path doesn't exist: $($FromFile)"
+        return
+    }
+
+    $AppsToInstall = Get-Content -Path $FromFile | Where-Object { $_[0] -ne "#" -and $null -ne $_[0] }
+    $CurrentlyInstalled = winget.exe list
+        
+    foreach ($Application in $AppsToInstall) {
+        $ApplicationName = $Application.Split(".")[1]
+        
+        Write-Host "Installing $ApplicationName... " -ForegroundColor Green -NoNewline
+        
+        if (($CurrentlyInstalled | Select-String -pattern $Application).Matches.Count -eq 0) {
+            try {
+                #TODO: winget.exe - remove comment stopping it from running
+                winget.exe install $Application | Out-Null
+                Write-Host "Complete!" -ForegroundColor Green   
+            }
+            catch {
+                Write-Host "Error occured..." -ForegroundColor Red
+            }
+        }
+        else {
+            Write-Host "Already Installed!" -ForegroundColor Yellow
+        }
+    }
+}
+
+Create-TempFolder
+Install-Fonts        -FromFile $Fonts_File
+Install-Applications -FromFile $Apps_EssentialsFile
+Install-Applications -FromFile $Apps_PersonalFile
